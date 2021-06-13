1a23ab95bab1cc2f7bec64f425527e4b89fda7d8
Deepanshu Sharma <73581139+Deepanshu291@users.noreply.github.com>
2021-06-13 18:37:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function hamburger(x) {
+    x.classList.toggle('active')
+    
+}
+
+// $(document).ready(function() {
+//     $(".menu").click(function() {
+//       $(".stick").toggleClass(function () {
+//         return $(this).is('.open, .close') ? 'open close' : 'open';
+//       });
+//     });
+//   });
+
+// $(document).ready(function(){
+// 	$('.menu-toggle').on('click',function(){
+// 		$('.menu-toggle').toggleClass('active');
+// 	});
+// });
